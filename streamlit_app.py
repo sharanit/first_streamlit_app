7501124fcbe7a9e8ee0d84642e14abe4c9924123
sharan vora <vorasharan18@gmail.com>
2023-05-19 18:00:50 +0530

    Update streamlit_app.py
    adding breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
-streamlit.title('MY Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Thepla')
-streamlit.text('Khandvi')
-streamlit.text('Khakhra')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
